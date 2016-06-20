7bcd8f3b1cc03841926d64b8bcf30a35c3ea2df6
Glenn Downing <downing@cs.utexas.edu>
2016-06-20 07:40:27 -0500

    another commit

--- examples/Lambdas.py
+++ examples/Lambdas.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#pylint: disable = bad-builtin, redefined-outer-name
+#pylint: disable = bad-builtin, function-redefined
 
 # ----------
 # Lambdas.py
@@ -13,59 +13,78 @@ print("Lambdas.py")
 def my_function (x, y) :
     return x + y
 
-f1 = my_function
+bf = my_function
 
 assert my_function(2, 3) == 5
-assert           f1(2, 3) == 5
+assert          bf(2, 3) == 5
 
 assert reduce(my_function, [2, 3, 4], 0) == 9
-assert reduce(f1,          [2, 3, 4], 0) == 9
+assert reduce(bf,          [2, 3, 4], 0) == 9
 
 
-f2 = lambda x, y : x + y
+
+bf = lambda x, y : x + y
 
 assert (lambda x, y : x + y)(2, 3) == 5
-assert f2(2, 3)                    == 5
+assert bf(2, 3)                    == 5
 
 assert reduce(lambda x, y : x + y, [2, 3, 4], 0) == 9
-assert reduce(f2,                  [2, 3, 4], 0) == 9
+assert reduce(bf,                  [2, 3, 4], 0) == 9
+
+
+
+def my_function () :
+    def f (x, y) :
+        return x + y
+    return f
+
+bf = my_function()
+
+assert my_function()(2, 3) == 5
+assert            bf(2, 3) == 5
+
+assert reduce(my_function(), [2, 3, 4], 0) == 9
+assert reduce(bf,            [2, 3, 4], 0) == 9
 
 
 
 def my_lambda () :
     return lambda x, y : x + y
 
-f3 = my_lambda()
+bf = my_lambda()
 
 assert my_lambda()(2, 3) == 5
-assert          f3(2, 3) == 5
+assert          bf(2, 3) == 5
 
 assert reduce(my_lambda(), [2, 3, 4], 0) == 9
-assert reduce(f3,          [2, 3, 4], 0) == 9
+assert reduce(bf,          [2, 3, 4], 0) == 9
 
 
 
-x = 2
+def my_closure (x) :
+    def f (y) :
+        return x + y
+    return f
 
-f4 = lambda y : x + y
+uf = my_closure(2)
 
-assert (lambda y : x + y)(3) == 5
-assert                 f4(3) == 5
+assert my_closure(2)(3) == 5
+assert            uf(3) == 5
 
-assert list(map(lambda y : x + y, [2, 3, 4])) == [4, 5, 6]
-assert list(map(f4,               [2, 3, 4])) == [4, 5, 6]
+assert list(map(my_closure(2), [2, 3, 4])) == [4, 5, 6]
+assert list(map(uf,            [2, 3, 4])) == [4, 5, 6]
 
 
 
 def my_closure (x) :
     return lambda y : x + y
 
-f5 = my_closure(2)
+uf = my_closure(2)
 
 assert my_closure(2)(3) == 5
-assert            f5(3) == 5
+assert            uf(3) == 5
 
 assert list(map(my_closure(2), [2, 3, 4])) == [4, 5, 6]
-assert list(map(f5,            [2, 3, 4])) == [4, 5, 6]
+assert list(map(uf,            [2, 3, 4])) == [4, 5, 6]
 
 print("Done.")
