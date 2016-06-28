72e75c368ce10390ff96a87d95dd132c27289f70
Glenn Downing <downing@cs.utexas.edu>
2016-06-28 16:07:44 -0500

    another commit

--- examples/FunctionUnpacking.py
+++ examples/FunctionUnpacking.py
@@ -14,14 +14,14 @@ def f (x, y, z) :
     return [x, y, z]
 
 t = (3, 4)
-assert f(2, 5, t)   == [2, 5, (3, 4)]
-assert f(2, *t)     == [2, 3, 4]
+assert f(2, 5, t) == [2, 5, (3, 4)]
+assert f(2, *t)   == [2, 3, 4]
+assert f(*t,  2)  == [2, 3, 4]
 assert f(z=2, *t) == [3, 4, 2]
 assert f(*t, z=2) == [3, 4, 2]
 
 # f(*t)       # TypeError: f() missing 1 required positional argument: 'z'
 # f(2, 3, *t) # TypeError: f() takes 3 positional arguments but 4 were given
-# f(*t,  2)   # SyntaxError: only named arguments may follow *expression
 # f(x=2, *t)  # TypeError: f() got multiple values for argument 'x'
 # f(*t,  x=2) # TypeError: f() got multiple values for argument 'x'
 
